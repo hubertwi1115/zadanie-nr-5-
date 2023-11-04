8808f4d40921be1ba07ba25eaf5c5fdd3779e34d
hubertwi1115 <148622055+hubertwi1115@users.noreply.github.com>
2023-11-04 14:45:56 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Podaj rok urodzenia:");
+        int rokUrodzenia = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Podaj imię i nazwisko:");
+        string imieNazwisko = Console.ReadLine();
+
+        Console.WriteLine("Podaj wiek:");
+        int wiek = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Podaj miejsce urodzenia:");
+        string miejsceUrodzenia = Console.ReadLine();
+
+        Console.WriteLine($"Dane osobowe: {rokUrodzenia}, {imieNazwisko}, {wiek} lat, {miejsceUrodzenia}");
+    }
+}
